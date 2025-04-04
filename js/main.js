f3c9d09cc0346566b1a2860a7f2893a399d2f9de
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-04 15:39:59 +0200

    aggiunta la struttura

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 const email = prompt('La tua email ?')
 console.log(email);
+const emailAccess = ['Iliad@email.com','vodafone@email.com ','tim@email.com','fastweb@email.com']
+console.log(emailAccess);
+let access = true;
+// controllo se l'email è presente nell'array emailAccess
+for (let i = 0; i < emailAccess.length; i++) {
+   
+    if (email === emailAccess[i]) {
+        access = true;
+    }
+    else if (email !== emailAccess[i]) {
+        access = false;
+    }
+}
+
