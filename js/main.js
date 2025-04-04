c0aeb2ac85ea9e79360a15d0f13fe41059bbf2d7
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-04 16:24:16 +0200

    fatte delle correzioni

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
-const email = prompt('La tua email ?')
-console.log(email);
-const emailAccess = ['Iliad@email.com','vodafone@email.com ','tim@email.com','fastweb@email.com']
+const email = prompt('La tua email ?');                   // creo la domanda per l'email;
+console.log(email);                                           
+
+const emailAccess = ["Iliad@email.com",                   // creo la lsta di email;
+    "Vodafone@email.com ",
+    "Tim@email.com",
+    "Fastweb@email.com"
+]     
+let access = true;                                        // creo una variabile d'accesso vera; 
+                                                                                   
 console.log(emailAccess);
-let access = true;
-// controllo se l'email è presente nell'array emailAccess
-for (let i = 0; i < emailAccess.length; i++) {
-   
-    if (email === emailAccess[i]) {
+
+for (let i = 0; i < emailAccess.length; i++){            //imposto il ciclo;                                   
+    console.log(emailAccess.length); 
+
+    if(emailAccess[i] === email) {                       // se la condizione è vera si accede;           
         access = true;
+        console.log(access);
+        alert('Accesso consentito'); 
+        console.log('Accesso consentito'); 
     }
-    else if (email !== emailAccess[i]) {
+    else if(emailAccess[i] !== email){                  // altrimenti se la condizione è falsa si nega l'accesso;
         access = false;
-    }
+        console.log(access);
+        alert('Accesso negato');
+        console.log('Accesso negato'); 
+    }  
 }
+                                                                               
+                                                                   
 
